a1fa8204655f6c5d5d3ed46f20de001d29c2d89b
movwf <movwf@protonmail.com>
2021-07-15 19:43:27 +0300

    UI: Search Results List component created.

---
+++ components/SearchResults/ResultsList.jsx
@@ -0,0 +1,11 @@
+import { DombulDOM } from 'dombul-dom';
+
+const ResultsList = (list) => {
+  return (
+    <div id="js-results-list" className="w-full flex flex-wrap justify-space-around px-5 md:px-10 lg:px-16">
+      
+    </div>
+  );
+};
+
+export default ResultsList;
--- components/SearchResults/index.jsx
+++ components/SearchResults/index.jsx
@@ -1,10 +1,25 @@
 import { DombulDOM } from 'dombul-dom';
 import SearchResultsHeader from './SearchResultsHeader';
+import ResultsList from './ResultsList';
+
+let list = [
+  {
+    idMeal: "52977",
+    strMeal: "Corba",
+    strArea: "Turkish",
+    strCategory: "Side",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/58oia61564916529.jpg",
+    strYoutube:"",
+    isFavorited:false
+  }
+];
 
 const SearchResults = () => {
   return (
     <main id="js-results" className="flex flex-col">
       {SearchResultsHeader()}
+      {ResultsList(list)}
     </main>
   );
 };
