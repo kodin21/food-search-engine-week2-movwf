c8fbd288031526b4d3fdcc5816df1383ea9ec984
movwf <movwf@protonmail.com>
2021-07-15 19:40:23 +0300

    UI: Search Results component created.

---
+++ components/SearchResults/index.jsx
@@ -0,0 +1,11 @@
+import { DombulDOM } from 'dombul-dom';
+
+const SearchResults = () => {
+  return (
+    <main id="js-results" className="flex flex-col">
+      
+    </main>
+  );
+};
+
+export default SearchResults;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { DombulDOM } from "dombul-dom";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import SearchResults from "./components/SearchResults";
 
 const App = () => {
   return (
     <div id="js-app" className="h-screen w-screen flex flex-col">
       {Header({ name: "John Doe" })}
       {SearchBar()}
+      {SearchResults()}
     </div>
   );
 };
