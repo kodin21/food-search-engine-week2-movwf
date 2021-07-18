d838b463eabd865b5cea5f02c79164815c22c74a
movwf <movwf@protonmail.com>
2021-07-18 08:27:31 +0300

    Util: Initial DOM update function updated with new initialResultsListRender function.

--- utils/initDOMUpdate.js
+++ utils/initDOMUpdate.js
@@ -1,13 +1,24 @@
 import MiniMealCard from "../components/MealCard/MiniMealCard";
+import { HEADER_USER_NAME_SELECTOR_ID } from "../config/constants";
 import { getUserName, getMealDB } from "../services/localStorage";
-import reRender from "../utils/reRender";
+import { initialResultsListRender } from "../utils/reRender";
+import sortByFavorite from "./sortByFavorite";
 
 export const initDOMUpdate = () => {
   // Update username with localStorage data
-  document.getElementById("js-user-name").innerText = getUserName();
+  document.getElementById(HEADER_USER_NAME_SELECTOR_ID).innerText =
+    getUserName();
+
+  // Local storage meal data sorted by favorites
+  let mealData = sortByFavorite(getMealDB());
 
   // Update search results with local storage
   // js-results-list -> Result List container ID
-  // js-52977 -> Rendered element ID
-  reRender("js-results-list", "js-52977", getMealDB(), MiniMealCard);
+  // js-52977 -> Rendered element ID (Initial Render With Example  Data)
+  initialResultsListRender(
+    "js-results-list",
+    "js-52977",
+    mealData,
+    MiniMealCard
+  );
 };
