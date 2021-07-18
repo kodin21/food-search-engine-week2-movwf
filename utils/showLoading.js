c265f9ba94fb71aaa7c75f4392633bfe548a71c4
movwf <movwf@protonmail.com>
2021-07-18 08:28:45 +0300

    Util: Loading screen togglers constant seperated.

--- utils/showLoading.js
+++ utils/showLoading.js
@@ -1,4 +1,5 @@
 import { DombulDOM } from "dombul-dom";
+import { LOADING_MODAL_ID } from "../config/constants";
 import Loading from "../components/Loading";
 
 const RootNode = document.getElementById("root");
@@ -8,7 +9,7 @@ export const showLoading = () => {
 };
 
 export const hideLoading = () => {
-  let removedNode = document.getElementById("js-loading");
+  let removedNode = document.getElementById(LOADING_MODAL_ID);
 
   RootNode.removeChild(removedNode);
 };
