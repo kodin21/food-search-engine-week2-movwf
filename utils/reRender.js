74ade1a348a0970bf4c004a817ee1a19096d440a
movwf <movwf@protonmail.com>
2021-07-18 08:24:02 +0300

    Util: Re-render fuctions renewed.

--- utils/reRender.js
+++ utils/reRender.js
@@ -1,33 +1,15 @@
 import { DombulDOM } from "dombul-dom";
 
-const reRender = (parentNodeId, nodeID, newNodes, newNodeComponent) => {
+export const reRender = (parentNodeId, nodeId, nodeComponent, newProps) => {
   let parentNode = document.getElementById(parentNodeId);
-  let prevNode = document.getElementById(nodeID);
+  let prevNode = document.getElementById(nodeId);
   // Remove the previous node
   parentNode.removeChild(prevNode);
 
-  // Render new Nodes
-  newNodes.map((newNode) =>
-    DombulDOM.render(newNodeComponent(newNode), parentNode)
-  );
+  parentNode.append(createDOMElement(nodeComponent(newProps)));
 };
 
-export const singleReRender = (
-  parentNodeId,
-  nodeID,
-  newProps,
-  newNodeComponent
-) => {
-  let parentNode = document.getElementById(parentNodeId);
-  let prevNode = document.getElementById(nodeID);
-  // Remove the previous node
-  parentNode.removeChild(prevNode);
-
-  // Render node with new props
-  DombulDOM.render(newNodeComponent(newProps), parentNode);
-};
-
-export const updateResultListRender = (
+export const initialResultsListRender = (
   parentNodeId,
   nodeID,
   newNodes,
@@ -39,7 +21,87 @@ export const updateResultListRender = (
   parentNode.removeChild(prevNode);
 
   // Render new Nodes
-  DombulDOM.render(newNodeComponent(newNodes), parentNode);
+  newNodes.forEach((node) => {
+    DombulDOM.render(newNodeComponent(node), parentNode);
+  });
 };
 
-export default reRender;
+export const updateResultListRender = (
+  parentNodeId,
+  nodeID,
+  nodeComponent,
+  newNodes
+) => {
+  let parentNode = document.getElementById(parentNodeId);
+  let prevNode = document.getElementById(nodeID);
+  // Remove the previous node
+  parentNode.removeChild(prevNode);
+
+  DombulDOM.render(nodeComponent(newNodes), parentNode);
+};
+
+export const insertReRender = (
+  parentNodeId,
+  nodeId,
+  nodeComponent,
+  newProps,
+  referenceId
+) => {
+  let parentNode = document.getElementById(parentNodeId);
+  let prevNode = document.getElementById(nodeId);
+  let refNode = document.getElementById(referenceId);
+  // Remove the previous node
+  parentNode.removeChild(prevNode);
+
+  parentNode.insertBefore(createDOMElement(nodeComponent(newProps)), refNode);
+};
+
+/*
+  =====================================
+    DOMBUL-DOM FUNCTIONS FOR CREATING
+    DOM ELEMENTS BY JSX COMPONENTS
+  =====================================
+*/
+
+export function createDOMElement(childNode) {
+  const { type, props } = childNode;
+
+  // Create DOM - Selective
+  const element =
+    type === "TEXT"
+      ? document.createTextNode(props.nodeValue)
+      : document.createElement(type);
+
+  // Spread props
+  Object.keys(props)
+    .filter(isAttribute)
+    .forEach((attribute) => {
+      element[attribute] = props[attribute];
+    });
+
+  // Spread listeners
+  Object.keys(props)
+    .filter(isListener)
+    .forEach((listener) => {
+      element.addEventListener(
+        listener.toLowerCase().substr(2), // Event Type : onClick -> "click"
+        props[listener] // Event callback : () => {}
+      );
+    });
+
+  // Append child elements
+  const childElements = props.children || [];
+  childElements.forEach((childElement) =>
+    createDOMElement(childElement, element)
+  );
+
+  return element;
+}
+
+function isListener(prop) {
+  return prop.startsWith("on");
+}
+
+function isAttribute(prop) {
+  return !isListener(prop) && prop !== "children";
+}
