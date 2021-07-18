4221d3d3bc1d4530b3ff03bae4ce9c32f65a7650
movwf <movwf@protonmail.com>
2021-07-18 08:10:52 +0300

    DOM id constants seperated.

---
+++ config/constants.js
@@ -0,0 +1,32 @@
+export const JS_EL_PREFIX = "js-";
+
+/*
+  =====================================
+    DOM_ELEMENT_SELECTORS
+  =====================================
+*/
+
+// HEADER
+export const HEADER_USER_NAME_SELECTOR_ID = JS_EL_PREFIX + "user-name";
+
+// SEARCH RESULTS
+export const SEARCH_RESULTS_CONTAINER_ID = JS_EL_PREFIX + "results";
+export const SEARCH_RESULTS_LIST_ELEMENT_ID = JS_EL_PREFIX + "results-list";
+
+// LOADING
+export const LOADING_MODAL_ID = "js-loading";
+
+// MODAL
+export const MODAL_BACKDROP_ID = "js-modal";
+
+// MINI MEAL CARD
+export const MINI_MEAL_CARD_FAVORITE_BUTTON_CONTAINER_PREFIX =
+  JS_EL_PREFIX + "fav-";
+export const MINI_MEAL_CARD_FAVORITE_BUTTON_PREFIX = JS_EL_PREFIX + "fav-btn-";
+
+// BIG MEAL CARD
+export const BIG_MEAL_CARD_FAVORITE_BUTTON_PREFIX =
+  JS_EL_PREFIX + "big-fav-btn-";
+export const BIG_MEAL_CARD_FAVORITE_BUTTON_CONTAINER_ID =
+  "js-big-fav-btn-container";
+export const BIG_MEAL_CARD_FAVORITE_BUTTON_REFERENCE_NODE_ID = "js-big-fav-ref";
