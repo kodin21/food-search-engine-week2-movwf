7119b4001a168ef8f661b606b44b2fe0cf774537
movwf <movwf@protonmail.com>
2021-07-18 08:34:06 +0300

    Util: Propagation effect tested : FAIL. Revoked to normal.

--- components/ModalBackdrop/index.jsx
+++ components/ModalBackdrop/index.jsx
@@ -1,14 +1,15 @@
-import { DombulDOM } from 'dombul-dom';
-import { closeModal } from '../../utils/toggleModal';
+import { DombulDOM } from "dombul-dom";
+import { closeModal } from "../../utils/toggleModal";
 
 const ModalBackdrop = (children) => {
   return (
     <div
       id="js-modal"
       className="fixed z-20 w-screen h-screen top-0 left-0 backdrop-filter backdrop-blur-sm flex flex-col justify-center items-center"
-      onClick={(e)=>{
+      onClick={(e) => {
         let modal = document.getElementById("js-modal");
-        if(e.target == modal){
+        // If only outside of modal
+        if (e.target == modal) {
           closeModal();
         }
       }}
