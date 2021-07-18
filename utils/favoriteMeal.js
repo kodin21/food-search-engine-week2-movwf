9269a6219a60f8b8377fca50057fcdd55030dede
movwf <movwf@protonmail.com>
2021-07-18 08:25:10 +0300

    Util: Favorite function updated. Re-rendering functions connected.

--- utils/favoriteMeal.js
+++ utils/favoriteMeal.js
@@ -1,6 +1,3 @@
-import BigMealCard from "../components/MealCard/BigMealCard";
-import MiniMealCard from "../components/MealCard/MiniMealCard";
-import ResultsList from "../components/SearchResults/ResultsList";
 import {
   updateFavoritedMeal,
   updateFavoriteMealDB,
@@ -8,7 +5,19 @@ import {
   getMealDataMealDB,
   getMealDB,
 } from "../services/localStorage";
-import { singleReRender, updateResultListRender } from "./reRender";
+import {
+  BIG_MEAL_CARD_FAVORITE_BUTTON_CONTAINER_ID,
+  BIG_MEAL_CARD_FAVORITE_BUTTON_REFERENCE_NODE_ID,
+  MINI_MEAL_CARD_FAVORITE_BUTTON_CONTAINER_PREFIX,
+  SEARCH_RESULTS_CONTAINER_ID,
+  SEARCH_RESULTS_LIST_ELEMENT_ID,
+} from "../config/constants";
+import { insertReRender, reRender, updateResultListRender } from "./reRender";
+import BigCardFavoriteButton from "../components/MealCard/BigMealCard/BigCardFavoriteButton";
+import MiniCardFavoriteButton from "../components/MealCard/MiniMealCard/MiniCardFavoriteButton";
+import sortByFavorite from "./sortByFavorite";
+import ResultsList from "../components/SearchResults/ResultsList";
+import MiniMealCard from "../components/MealCard/MiniMealCard";
 
 export const favoriteMeal = (mealID, nodeID) => {
   // If meal in mealDB favorited then remove it from favorite-meals
@@ -17,15 +26,34 @@ export const favoriteMeal = (mealID, nodeID) => {
   // Toggle the mealDB isFavorited data
   updateFavoriteMealDB(mealID);
 
+  // Check if called by meal card modal
   let isBig = nodeID.startsWith("js-big-");
-  let NodeComponent = isBig ? BigMealCard : MiniMealCard;
+
+  // Selected meal data
+  let { idMeal, isFavorited } = getMealDataMealDB(mealID);
 
   isBig
-    ? singleReRender("js-modal", nodeID, getMealDataMealDB(mealID), BigMealCard)
-    : updateResultListRender(
-        "js-results",
-        "js-results-list",
-        getMealDB(),
-        ResultsList
+    ? insertReRender(
+        BIG_MEAL_CARD_FAVORITE_BUTTON_CONTAINER_ID,
+        nodeID,
+        BigCardFavoriteButton,
+        { idMeal, isFavorited },
+        BIG_MEAL_CARD_FAVORITE_BUTTON_REFERENCE_NODE_ID
+      )
+    : reRender(
+        MINI_MEAL_CARD_FAVORITE_BUTTON_CONTAINER_PREFIX + mealID,
+        nodeID,
+        MiniCardFavoriteButton,
+        { idMeal, isFavorited }
       );
+
+  // Re-render updated result list
+  let mealData = sortByFavorite(getMealDB());
+
+  updateResultListRender(
+    SEARCH_RESULTS_CONTAINER_ID,
+    SEARCH_RESULTS_LIST_ELEMENT_ID,
+    ResultsList,
+    mealData
+  );
 };
