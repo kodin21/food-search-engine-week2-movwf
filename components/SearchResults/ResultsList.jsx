801ef3525f4711924aa9b34f78c8cc8e6bd3dae4
movwf <movwf@protonmail.com>
2021-07-18 08:36:25 +0300

    UI: Result list selecting id set.

--- components/SearchResults/ResultsList.jsx
+++ components/SearchResults/ResultsList.jsx
@@ -1,9 +1,12 @@
-import { DombulDOM } from 'dombul-dom';
-import MiniMealCard from '../MealCard/MiniMealCard';
+import { DombulDOM } from "dombul-dom";
+import MiniMealCard from "../MealCard/MiniMealCard";
 
 const ResultsList = (list) => {
   return (
-    <div id="js-results-list" className="w-full flex flex-wrap justify-space-around px-5 md:px-10 lg:px-16">
+    <div
+      id="js-results-list"
+      className="w-full flex flex-wrap justify-space-around px-5 md:px-10 lg:px-16"
+    >
       {list.map(MiniMealCard)}
     </div>
   );
